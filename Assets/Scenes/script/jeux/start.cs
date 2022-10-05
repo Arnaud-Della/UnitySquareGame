6deeb6ba828bc55778469c15ba75d489618f6924
Ludovic Brieulle <ludovic.brieulle@devoteam.lu>
2022-10-05 15:43:13 +0200

    finish color

--- Assets/Scenes/script/jeux/start.cs
+++ Assets/Scenes/script/jeux/start.cs
@@ -7,11 +7,10 @@ public class start : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject Cube;
-    public GameObject Sol;
     void Start()
     {
-        Instantiate(Cube,new Vector3(-8,0,0),Quaternion.identity);
-        Sol.GetComponent<SpriteRenderer>().color = Color.black;
+        Cube=Instantiate(Cube,new Vector3(-8,0,0),Quaternion.identity);
+        Cube.GetComponent<SpriteRenderer>().color = new Color((float)((float)Random.Range(0, 100)/100), (float)((float)Random.Range(0, 100) / 100), (float)((float)Random.Range(0, 100) / 100), 1);
     }
 
     // Update is called once per frame

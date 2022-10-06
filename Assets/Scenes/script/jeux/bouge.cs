38fd82622c731bf25b3223ccdcdcf678839254d4
Ludovic Brieulle <ludovic.brieulle@devoteam.lu>
2022-10-06 10:55:17 +0200

    conect 2Cubefinish1

--- Assets/Scenes/script/jeux/bouge.cs
+++ Assets/Scenes/script/jeux/bouge.cs
@@ -19,9 +19,17 @@ public class bouge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             this.transform.position = new Vector2(transform.position.x + 1 * Time.deltaTime, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position = new Vector2(transform.position.x - 1 * Time.deltaTime, transform.position.y);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position = new Vector2(transform.position.x , transform.position.y + 10 * Time.deltaTime);
         }
     }
 }
